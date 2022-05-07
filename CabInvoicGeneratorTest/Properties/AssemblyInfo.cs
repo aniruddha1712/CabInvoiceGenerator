6a0dd041b021aa21d264e7cf96dbb9f34a2e29e6
Aniruddha <aniruddha1712@gmail.com>
2022-05-07 16:42:04 +0530

    [Aniruddha]Added: calculate fare for single ride

---
+++ CabInvoicGeneratorTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CabInvoicGeneratorTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("CabInvoicGeneratorTest")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1fcc3f2f-99cf-4873-9a13-e05d78751659")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- CabInvoiceGenerator/Program.cs
+++ CabInvoiceGenerator/Program.cs
@@ -11,6 +11,9 @@ namespace CabInvoiceGenerator
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Cab Invoice Generator Program");
+
+            CabInvoiceGen cabInvoiceGenerator = new CabInvoiceGen(RideType.NORMAL);
+            Console.WriteLine(cabInvoiceGenerator.CalculateFare(10, 15));
             Console.ReadLine();
         }
     }
